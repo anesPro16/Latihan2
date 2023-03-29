d1b822612d24bb271bde11eee3f2d67b2dd59228
anesPro16 <vayohputra@gmail.com>
2023-03-29 21:46:51 +0700

    tambah br

--- view-latihan.php
+++ view-latihan.php
@@ -6,8 +6,8 @@
 <body>
 	<!-- // pertemuan 3 -->
 Halo Kawan.. Yuk kita belajar web programming..!! <br>
-Nilai 1 = <?= $nilai1.' '; ?>
-Nilai 2 = <?= $nilai2.' '; ?>
+Nilai 1 = <?= $nilai1.' '; ?> <br>
+Nilai 2 = <?= $nilai2.' '; ?> <br>
 ini hasil dari pemodelan dengan metode pernjumlahan yaitu <?= "{$nilai1} + {$nilai2} =  {$hasil}"; ?>
 
 <!-- $this->load->view('view-latihan1', $data); -->
